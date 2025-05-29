5dfc817eaa5de1956c9694d6886527e1ffce8a44
iylee <iylee153@gmail.com>
2025-05-29 12:59:15 +0900

    cicd first commit

---
+++ 4_2_text.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+print("Hello World!")
+print('''
+      Hello
+      World!
+      ''')
+
+print("'1'+'1'", '1'+'1')  # string concatenation
+print('Hello world'*1000)
+print("len('Hello world'*1000)", len('Hello world'*1000))  # length of string
+
+print("'Hello world'.replace('world', 'universe')", 'Hello world'.replace('world', 'universe'))
